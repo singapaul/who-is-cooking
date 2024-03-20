d5daf49c2228e1517564d6cac264ff19b6b10e31
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-03-20 18:14:02 +0000

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+ 
+</script>
+
+<main class='w-screen h-screen flex flex-col items-center justify-center gap-4' >
+    <h1 class="text-2xl">BOSH YOU'VE HIT A {$page.status}: {$page.error?.message}</h1>
+ <img class="max-w-48" src={'/bosh-big-john.gif'} />
+</main>
